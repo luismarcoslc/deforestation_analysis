464a4d154edfc7dd83dc9c32b9905ea219eed971
Luis Marcos López <luismarcoslc@gmail.com>
2022-11-19 09:37:57 +0100

    load dataset

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+from pandas import read_csv
+import numpy as np
+import keras
+
+
+if __name__ == '__main__':
+    train_path = os.path.join(os.getcwd(), 'train.csv')
+    test_path = os.path.join(os.getcwd(), 'test.csv')
+    testdf = read_csv(train_path)
+    traindf = read_csv(test_path)
+    print(traindf)
